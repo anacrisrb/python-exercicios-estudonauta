66c29d3cff175398481add945e12a746f7438bae
AnaCristina <anacrisribeirost@gmail.com>
2026-01-03 16:23:48 -0300

    add novos ex - MUNDO 02

--- MUNDO 02/ex043.py
+++ MUNDO 02/ex043.py
@@ -1,7 +1,24 @@
 # Desenvolva uma lógica  que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+from sys import float_repr_style
 
 # Abaixo de 18.5: Abaixo do peso
 # Entre 18.5 e 25: Peso ideal
 # 25 até 30: Sobrepeso
 # 30 até 40: Obesidade
 # Acima de 40: Obesidade mórbida
+
+peso = float(input("Informe o seu peso: "))
+altura = float(input("Informe a sua altura: "))
+
+imc = peso / (altura**2)
+
+if imc < 18.5:
+    print("Abaixo do peso")
+elif 18.5 <= imc < 25:
+    print("Peso ideal")
+elif 25 <= imc < 30:
+    print("Sobrepeso")
+elif 30 <= imc < 40:
+    print("Obesidade")
+else:
+    print("Obesidade mórbida")
--- MUNDO 02/ex044.py
+++ MUNDO 02/ex044.py
@@ -4,3 +4,66 @@
 # À vista no cartão: 5% de desconto
 # Em até 2x no cartão: preço normal
 # 3x ou mais no cartão: 20% de juros
+
+print("-=-==" * 4)
+print("\033[1;30;43mMENU DE PRODUTOS:\033[m")
+print("-=-==" * 4)
+
+print("[ 1 ] Bicicleta")
+print("[ 2 ] Iphone 16 pro max")
+print("[ 3 ] Mochila Adidas")
+print("[ 4 ] Chuteira")
+
+while True:
+    produto = int(input("\033[1;31mInforme o produto desejado: \033[m"))
+
+    if produto == 1:
+        print("Bicicleta")
+        preco = 900.00
+        break
+    elif produto == 2:
+        print("Iphone 16 Pro Max")
+        preco = 1500.00
+        break
+    elif produto == 3:
+        print("Mochila Adidas")
+        preco = 300.00
+        break
+    elif produto == 4:
+        print("Chuteira")
+        preco = 450.00
+        break
+    else:
+        print("❌ Produto inválido. Tente novamente.\n")
+
+
+print("-=-==" * 5)
+print("\033[1;30;43mCONDIÇOES DE PAGAMENTO: \033[m")
+print("-=-==" * 5)
+
+print("[ 1 ] A vista no dinheiro ou cheque: 10% de desconto")
+print("[ 2 ] A vista no cartão: 5% de desconto")
+print("[ 3 ] Em até 2x no cartão: preço normal")
+print("[ 4 ] 3X ou mais no cartão: 20% de juros")
+
+while True:
+    pagamento = int(input("\033[1;31mComo o produto será pago? \033[m"))
+
+    if pagamento == 1:
+        print(f"Pagamento à vista (dinheiro/cheque). Total: R$ {preco * 0.90:.2f}")
+        break
+    elif pagamento == 2:
+        print(f"Pagamento à vista no cartão. Total: R$ {preco * 0.95:.2f}")
+        break
+    elif pagamento == 3:
+        print(f"Pagamento em até 2x no cartão. Total: R$ {preco:.2f}")
+        break
+    elif pagamento == 4:
+        print(f"Pagamento em 3x ou mais no cartão. Total: R$ {preco * 1.20:.2f}")
+        break
+    else:
+        print("❌ Forma de pagamento inválida. Tente novamente.\n")
+
+
+
+
