4829738c9eefcc130522dffc30299a913fefc82e
AnaCristina <anacrisribeirost@gmail.com>
2026-03-30 21:29:24 -0300

    Update ex resolvidos

--- MUNDO 02/ex064.py
+++ MUNDO 02/ex064.py
@@ -1,3 +1,17 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 
 # No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+
+cont = 0
+soma = 0
+
+n = int(input("Digite um número: "))
+
+while n != 999:
+    cont += 1
+    soma += n
+    n = int(input("Digite um número: "))
+
+print(f"Parando o programa.")
+print(f"Foram digitados {cont} números", end=" ")
+print(f"e  soma entre o números digitados é igual a: {soma}")
--- MUNDO 02/ex065.py
+++ MUNDO 02/ex065.py
@@ -1,3 +1,32 @@
-# Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e menor valores lidos.
-
+# Crie um programa que leia vários números inteiros pelo teclado.
+# No final da execução, mostre a média entre todos os valores e qual foi o maior e menor valores lidos.
 # O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+cont = 0
+soma = 0
+num = 0
+resp = "S"
+
+while resp == "S":
+    n = int(input(f"Digite um número: "))
+    cont += 1
+    soma += n
+
+    if cont == 1:  # Validar a comparação com um unico numero
+        maior = n
+        menor = n
+    else:
+        if n > maior:
+            maior = n
+        elif n < menor:
+            menor = n
+
+    resp = input("Quer continuar? [S/N] ").strip().upper()
+    while resp not in "SN":
+        resp = input(f"Opção inválida. Deseja continuar? [S/N]").strip().upper()
+
+media = soma / cont
+
+print("A média entre o números digitados foi {:.0f}".format(media))
+print("O MAIOR valor digitado foi {}".format(maior), end=" ")
+print("e o MENOR valor digitado foi {}.".format(menor))
