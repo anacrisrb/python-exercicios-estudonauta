10e6ccfe112f94314e126631e5b86cdcf83a52e8
AnaCristina <anacrisribeirost@gmail.com>
2026-03-30 21:29:08 -0300

    Update ex mundo 02

--- MUNDO 02/ex062.py
+++ MUNDO 02/ex062.py
@@ -1,1 +1,33 @@
-# Melhora o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando  ele disser que quer mostrar 0 termos.
+# Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando  ele disser que quer mostrar 0 termos.
+
+primeiro_termo = int(input("Digite o primeiro termo: "))
+razao = int(input("Informe a razao: "))
+
+termo = primeiro_termo
+contador = 1
+total = 10
+
+print(
+    "Os 10 primeiros termos da PA são: "
+)  # Termo digitado mais a razao // iniciando no primeiro
+
+while contador <= 10:
+    print(termo, end=" ")  # end para imprimir tudo na mesma linha (separado por espaço)
+    termo += razao
+    contador += 1  # Vai finalizar quando chegar no décimo
+
+print()
+mais = int(input(f"Quantos termos mais deseja mostrar? "))
+
+while mais != 0:
+    total += mais  # aumenta o total de termos
+
+    while contador <= total:
+        print(termo, end=" ")
+        termo += razao
+        contador += 1
+
+    print()
+    mais = int(input("Quantos termos mais deseja mostrar? "))
+
+print("\033[1;31;40mPrograma encerrado.\033[m")
--- MUNDO 02/ex063.py
+++ MUNDO 02/ex063.py
@@ -3,3 +3,18 @@
 # Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 
 # A sequência de Fibonacci é uma sequência de números em que cada termo (a partir do terceiro) é a soma dos dois anteriores.
+
+n = int(input(f"Informe um número: "))
+
+contador = 1
+t1 = 0
+t2 = 1
+
+print(f"Os {n} primeiros termos de Fibonacci são:")
+
+while contador <= n:
+    print(t1, end=" ")
+    t3 = t1 + t2  # T3 é a soma dos dois elementos anteriores
+    t1 = t2
+    t2 = t3
+    contador += 1
